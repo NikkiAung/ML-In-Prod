f636adc231c57ed94c989ed36bf9f5f0b8ebe541
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-12 15:04:10 -0800

    exploring around Flask and pipenv

---
+++ 2_test_flask_and_pipenv/app.py
@@ -0,0 +1,12 @@
+# save this as app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    str_out = "This is {} server : {}".format(os.getenv("ENV"),os.getenv("VER"))
+    return str_out
+
+app.run()
